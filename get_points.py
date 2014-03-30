103ef1910a8a43b22bb3e283023f844aa9f9034b
manuzope <manuzope@gmail.com>
2014-03-30 14:12:43 -0400

    get_points

---
+++ get_points.py
@@ -0,0 +1,36 @@
+from SimpleCV import *
+
+def get_points(img):
+    disp = Display()
+    points = []
+    while(disp.isNotDone()):
+        if (disp.mouseLeft and disp.leftButtonDownPosition() != None):
+            x = disp.mouseX
+            y = disp.mouseY
+            point = (x, y)
+            print point
+            points.append(point)
+            if len(points) == 6:
+                disp.done = True
+
+        img.save(disp)
+
+    print points
+    return points
+
+def get_filename(fn):
+    f = fn.split("/")
+    f = f[len(f) - 1]
+    f = f.split(".")[0]
+    return f
+
+iset = ImageSet("images/terry")
+image_dict = {}
+
+for img in iset:
+    fn = get_filename(img.filename)
+    points = get_points(img)
+    image_dict[fn] = points
+
+print image_dict
+ 
